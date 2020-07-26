9ee26cb63e322d1595148a9841528ba4d8d36323
Redari-Es <905422030@qq.com>
2020-07-26 15:06:26 +0800

    this is record learning web for me

---
+++ 表单邮箱/welcome.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>一个最简单的表单<title>
+</head>
+<body>
+	<form action="welcome.php" method="post">
+		名字：<input type="text" name="name"><br><br>
+		邮箱：<input type="text" name="emai"><br><br>
+			<button type="submit">提交</button>
+	</form>
+	
+</body>
+
+</html>
